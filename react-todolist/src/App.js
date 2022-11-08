ab02fe48def376cda425662cca6b11bcd11a9ef4
AshkanMahdizadeh <ac.programmer.ashkan@gmail.com>
2022-11-08 17:52:40 +0330

    setup react-todolist project

---
+++ react-todolist/src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Start</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
