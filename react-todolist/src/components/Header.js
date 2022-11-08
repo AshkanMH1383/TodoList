41b32313832822d499b77ab28fca03087b5e7496
AshkanMahdizadeh <ac.programmer.ashkan@gmail.com>
2022-11-08 18:29:09 +0330

    Add Header and add fonts

--- react-todolist/src/components/Header.js
+++ react-todolist/src/components/Header.js
@@ -1,7 +1,11 @@
 function Header() {
     return (
       <header className="Header">
-        <h1>header</h1>
+        <h1>لیست انجام کارها</h1>
+         <ul className="box-menu">
+            <li className="active"><span href="">کار ها</span></li>
+            <li><span href="">درحال انجام دادن</span></li>
+         </ul>
       </header>
     );
   }
